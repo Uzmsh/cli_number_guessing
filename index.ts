2148807261f357e977f437d3efb96482d108fc0a
Uzma Shahzad <93823334+Uzmsh@users.noreply.github.com>
2024-04-05 12:47:29 +0500

    creatted number guessing game

---
+++ index.ts
@@ -0,0 +1,21 @@
+ import inquirer from "inquirer";
+
+ const randomNumber = Math.floor(Math.random()*6+1);
+
+
+
+const answers = await inquirer.prompt([
+    {
+        name:"userGuessNumber",
+        type : "number",
+        message : "Please guess a number between 1-25: ",
+    }
+]);
+console.log(answers);
+
+if(answers.userGuessNumber === randomNumber){
+    console.log("congratulations you guessed right number.")
+} else {
+    console.log("you guessed wrong number.");
+}
+console.log(randomNumber);
